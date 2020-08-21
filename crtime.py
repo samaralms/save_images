9a774d7a1618f4b78f039e72cf6e0bb8b856bbe4
samaralms <al1609086@upy.edu.mx>
2020-08-21 08:44:19 -0500

    archivo de guardar imagenes

--- crtime.py
+++ crtime.py
@@ -2,6 +2,8 @@
 from stat import S_ISREG, ST_CTIME, ST_MODE
 import os, sys, time
 
+# ST_MODE: Inode protection mode.
+
 # path to the directory (relative or absolute)
 dirpath = sys.argv[1] if len(sys.argv) == 2 else r'/home/samara/Desktop/OPTIMOTION2/Proyectos_Relevantes/guardar_img/img_folder'
 
@@ -19,9 +21,8 @@ entries = ((stat[ST_CTIME], path)
 myimages = []
 for cdate, path in sorted(entries):
     var = (time.ctime(cdate), os.path.basename(path))
-    #print (time.ctime(cdate), os.path.basename(path))
-    myimages.append(var)    
-    print(myimages)
+    #print (time.ctime(cdate), os.path.basename(path))   
+    print(var)
 
 
     
